0cea48c9aa4896a08fd7fea35ec350f73cfddb02
fernandoc1 <fernandoc@epidataconsulting.com>
2020-09-30 22:12:35 -0300

    primer commit

---
+++ mercadopago.js
@@ -0,0 +1,28 @@
+
+// SDK de Mercado Pago
+const mercadopago = require ('mercadopago');
+
+// Agrega credenciales
+mercadopago.configure({
+  access_token: 'APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'
+});
+
+// Crea un objeto de preferencia
+let preference = {
+  items: [
+    {
+      title: 'Mi producto',
+      unit_price: 100,
+      quantity: 1,
+    }
+  ]
+};
+
+mercadopago.preferences.create(preference)
+.then(function(response){
+// Este valor reemplazará el string "<%= global.id %>" en tu HTML
+  global.id = response.body.id;
+
+}).catch(function(error){
+  console.log(error);
+});
